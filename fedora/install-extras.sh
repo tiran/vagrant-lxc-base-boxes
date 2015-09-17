38190a9123fa4ffb66f154f185b3a0a3e692e13b
Christian Heimes <christian@python.org>
2015-09-17 20:02:15 +0200

    Install more packages for Dogtag

--- fedora/install-extras.sh
+++ fedora/install-extras.sh
@@ -18,10 +18,12 @@ sleep $SECS
 PACKAGES=(vim curl wget man-db bash-completion ca-certificates sudo openssh-server)
 
 # extra stuff for Dogtag PKI
-EXTRA_PACKAGES=(httpd jackson java-headless jss ldapjdk libselinux-python \
+EXTRA_PACKAGES=( \
+    389-ds-base httpd jackson java-headless jss ldapjdk libselinux-python \
     nss nss-tools nuxwdog openldap-clients policycoreutils-python python-ldap \
-    python-lxml python-requests python-six resteasy-client selinux-policy \
-    selinux-policy-targeted tomcat xalan-j2 xerces-j2)
+    python-lxml python-requests python-six resteasy-client rng-tools \
+    selinux-policy selinux-policy-targeted tomcat xalan-j2 xerces-j2 \
+    yum-utils)
 
 utils.lxc.attach yum update -y
 utils.lxc.attach yum install ${PACKAGES[*]} -y
